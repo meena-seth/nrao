04f156793d68a301b9514f70eb02345d85825f2c
mseth <mseth2@uw.edu>
2025-07-21 12:41:01 -0400

    update

--- trying_on_cedar/trying_two.py
+++ trying_on_cedar/trying_two.py
@@ -13,7 +13,13 @@ beam = beam_dset[:] # using the square brackets, beam is now a numpy array of sh
 # axes labels are provided in the Dataset attrs if needed
 axes_names = beam_dset.attrs["axis"]
 
+index_map = f["index_map"]
+
+
 # individual cylinder and polarization slices; these go into the third axis above
 cylDy = slice(1536, 1792)
 # Take a look at what's in the slice 
 print(f"cylDy is: {cylDy}")
+
+cylDy_indexmap = index_map["cylDy"][:]
+print(cylDy_indexmap)

e74d116cdf3c8e54b17f214957cf55628ebf6a6e
mseth <mseth2@uw.edu>
2025-07-21 09:40:28 -0400

    update

---
+++ hello.py
@@ -0,0 +1,6 @@
+msg = "Roll a dice!"
+print(msg)
+
+import numpy as np
+
+print(np.random.randint(1,9))
---
+++ trying_on_cedar/trying_one.py
@@ -0,0 +1,43 @@
+import h5py
+
+filename = "/project/rpp-chime/areda26/stuff_for_other_people/hsiu-hsien/TauA_105/2667/"
+
+f = h5py.File(filename, "r")
+
+beam_dset = f["beam"] # beam_dset is an instance of h5py.Dataset
+beam = beam_dset[:] # using the square brackets, beam is now a numpy array of shape (1024, 2, 2048, 2160)
+
+# axes labels are provided in the Dataset attrs if needed
+axes_names = beam_dset.attrs["axis"]
+
+# there is a corresponding weight dataset if needed:
+weight = f["weight"][:] # same shape as above; weight is defined as 1 / sigma^2 i.e. inverse variance
+
+# individual cylinder and polarization slices; these go into the third axis above
+cylAy = slice(0, 256)
+cylBy = slice(512, 768)
+cylCy = slice(1024, 1280)
+cylDy = slice(1536, 1792)
+
+cylAx = slice(256, 512)
+cylBx = slice(768, 1024)
+cylCx = slice(1280, 1536)
+cylDx = slice(1792, 2048)
+
+# the axis definitions are supplied in `index_map`
+index_map = f["index_map"]
+
+# get the hour angle axis
+ha = index_map["pix"]["phi"][:]
+
+# get the frequency axis
+freq = index_map["freq"][:]
+
+# if the declination is needed, for instance for calculating degrees on sky from hour angle, there's an attribute for that
+dec = f.attrs["dec"]
+
+print(f" The beam is an array with shape {beam.shape}.")
+print(f" The axes names are {axes_names}.")
+print(f"The weight dataset is an array with shape {weight.shape}.")
+print(f"The index map is {index_map}.")
+print(f"The ha is {ha}, the dec is {dec}, and the frequency axis is {freq}.")

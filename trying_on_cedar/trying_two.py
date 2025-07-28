cdcf66080a63f4339b8a0040b7cc37f572cd8e47
mseth <mseth2@uw.edu>
2025-07-28 16:18:31 -0400

    update

--- trying_on_cedar/trying_two.py
+++ trying_on_cedar/trying_two.py
@@ -24,14 +24,14 @@ cylDy_slice = slice(1536, 1792)
 cylDy = beam_dset[:,:,cylDy_slice,:]
 
 # Make the slice I want 
-cylDy_fre716 = cylDy[freq_idx, 0, 9, :]  # Values for every HA at freq=716
+cylDy_fre716 = cylDy[freq_idx, 1, 9, :]  # Values for every HA at freq=716
 cylDy_fre716 = np.abs(cylDy_fre716)/np.max(np.abs(cylDy_fre716))
 
 # Plot 
 fig, ax = plt.subplots(constrained_layout=True, figsize=(10,5))
-ax.scatter(has, cylDy_fre716, s=0.5)
+ax.plot(has, cylDy_fre716, s=0.5)
 ax.set_xlabel("HA")
 ax.set_ylabel("Relative sensitivity?")
 ax.set_yscale("log")
-ax.set_title("CylD, yy pol, beam 9, 716 MHz")
-fig.savefig("Plot_One")
+ax.set_title("CylD, yx pol, beam 9, 716 MHz")
+fig.savefig("Plot_Two")

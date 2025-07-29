7aae58f7fea52876dd0be9107a1829911fcd0e53
mseth <mseth2@uw.edu>
2025-07-29 11:56:29 -0400

    update

--- trying_on_cedar/trying_two.py
+++ trying_on_cedar/trying_two.py
@@ -8,6 +8,8 @@ f = h5py.File(filename, "r")
 import pdb; pdb.set_trace()
 beam_dset = f["beam"] 
 
+import pdb; pdb.set_trace()
+
 # Read axes names & make array for freqs and HAs
 axes_names = beam_dset.attrs["axis"]
 index_map = f["index_map"]

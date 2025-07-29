99516c9ddfbad20c5d8ec278a7eba961dd787878
mseth <mseth2@uw.edu>
2025-07-29 13:11:57 -0400

    update

--- trying_on_cedar/trying_two.py
+++ trying_on_cedar/trying_two.py
@@ -5,10 +5,7 @@ import matplotlib.pyplot as plt
 # Load in the data file
 filename = "/project/rpp-chime/areda26/stuff_for_other_people/hsiu-hsien/TauA_105/2667/TAU_A_2667_20181014T120212.h5"
 f = h5py.File(filename, "r")
-import pdb; pdb.set_trace()
 beam_dset = f["beam"] 
-
-import pdb; pdb.set_trace()
 
 # Read axes names & make array for freqs and HAs
 axes_names = beam_dset.attrs["axis"]
@@ -30,10 +27,10 @@ cylDy_fre716 = cylDy[freq_idx, 1, 9, :]  # Values for every HA at freq=716
 cylDy_fre716 = np.abs(cylDy_fre716)/ np.max(np.abs(cylDy_fre716))
 cylDy_plot2 = np.square(cylDy_fre716)
 
-cylDy[freq_idx, 1, 9, :] 
+#cylDy[freq_idx, 1, 9, :] 
 
-cylDy_abs = np.abs(cylDy_fre716 * cylDy_fre716.conj())
-cylDy_plot = cylDy_abs / np.max(cylDy_abs)
+#cylDy_abs = np.abs(cylDy_fre716 * cylDy_fre716.conj())
+#cylDy_plot = cylDy_abs / np.max(cylDy_abs)
 
 #cylDy_fre716 = np.abs(cylDy_fre716)/np.max(np.abs(cylDy_fre716))
 
@@ -44,3 +41,4 @@ ax.set_xlabel("HA")
 ax.set_ylabel("Relative sensitivity?")
 ax.set_yscale("log")
 ax.set_title("CylD, yx pol, beam 9, 716 MHz")
+plt.savefig("Figure")
